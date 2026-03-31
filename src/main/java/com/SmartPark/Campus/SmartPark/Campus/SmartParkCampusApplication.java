2fd12cd2a60478271b89f464ecb07b307b02011f
Chamath Dissanayake <chamathjeew@gmail.com>
2026-03-31 23:29:13 +0530

    Initial commit

---
+++ src/main/java/com/SmartPark/Campus/SmartPark/Campus/SmartParkCampusApplication.java
@@ -0,0 +1,13 @@
+package com.SmartPark.Campus.SmartPark.Campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SmartPark/Campus/SmartPark/Campus/SmartParkCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartPark.Campus.SmartPark.Campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
